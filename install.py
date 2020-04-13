8a66f48a0c226d36261f7cb01776672b0fc1ec6e
Rafael M. Martins <rafael.martins@lnu.se>
2020-04-13 10:26:15 +0200

    Some basic functionalities are up

--- install.py
+++ install.py
@@ -5,10 +5,9 @@ import ctypes
 
 # https://gist.github.com/f0k/63a664160d016a491b2cbea15913d549
 def check_for_cuda():
-    libnames = ['libcuda.so', 'libcuda.dylib', 'cuda.dll']
-    for libname in libnames:
+    for libname in ['libcuda.so', 'libcuda.dylib', 'cuda.dll']:
         try:
-            cuda = ctypes.CDLL(libname)
+            ctypes.CDLL(libname)
         except OSError:
             continue
         else:
@@ -16,13 +15,18 @@ def check_for_cuda():
     return False
 
 
+def pip_install(module_name):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', module_name])
+
 if __name__ == '__main__':    
     dep = sys.argv[1]
 
     if dep == "ivis":
         cpu_or_gpu = 'gpu' if check_for_cuda() else 'cpu'
-        module_name = f"ivis[{cpu_or_gpu}]"
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', module_name])
+        pip_install(f"ivis[{cpu_or_gpu}]")        
+
+    if dep == 'umap':
+        pip_install('umap-learn')
     
     else:
         print("Unknown dependency:", dep)
