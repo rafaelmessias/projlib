d0c14b570584b6401ba76dcb79d9d8726d593f8d
Rafael M. Martins <rafael.martins@lnu.se>
2020-05-12 11:40:53 +0200

    Small improvements for TW

--- quality/trustworthiness.py
+++ quality/trustworthiness.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 from projlib.quality.metric import DistanceBasedMetric
+from numba import jit
+from setdiff1d import setdiff1d
+from sklearn.neighbors import NearestNeighbors
 
 
 def trustworthiness(D_high, D_low, k=7):
@@ -20,11 +23,41 @@ def trustworthiness(D_high, D_low, k=7):
 
         sum_j = 0
         for j in range(U.shape[0]):
-            sum_j += np.where(nn_orig[i] == U[j])[0] - k
+            sum_j += np.where(nn_orig[i] == U[j])[0][0] - k
 
         sum_i += sum_j
 
-    return float((1 - (2 / (n * k * (2 * n - 3 * k - 1)) * sum_i)).squeeze())
+    return 1 - (2 / (n * k * (2 * n - 3 * k - 1)) * sum_i)
+
+
+# TODO The numba version is performing bad right now. Maybe it's because sorting in numba is slower than in numpy, and it also does not support arrays with more than 1D. Also my implementation of setdiff1d is not very good.
+
+@jit(nopython=True, parallel=True)
+def numba_tw(D_high, D_low, k=7):
+    n = D_high.shape[0]
+    
+    nn_orig, nn_proj = np.empty_like(D_high), np.empty_like(D_low)
+    for i in range(n):
+        nn_orig[i] = D_high[i].argsort()
+        nn_proj[i] = D_low[i].argsort()
+
+    knn_orig = nn_orig[:, 1:k + 1]
+    knn_proj = nn_proj[:, 1:k + 1]
+
+    sum_i = 0
+
+    for i in range(n):
+        #U = np.setdiff1d(knn_proj[i], knn_orig[i])
+        U = setdiff1d(knn_proj[i], knn_orig[i])
+
+        sum_j = 0
+        for j in range(U.shape[0]):
+            v = np.where(nn_orig[i] == U[j])[0][0]            
+            sum_j += v - k
+
+        sum_i += sum_j
+
+    return 1 - (2 / (n * k * (2 * n - 3 * k - 1)) * sum_i)
 
 
 # TODO  Make importing a bit easier; right now it's:
@@ -49,6 +82,7 @@ class Trustworthiness(DistanceBasedMetric):
 
             sum_j = 0
             for j in range(U.shape[0]):
+                # This is so stupid...
                 sum_j += np.where(nn_orig[i] == U[j])[0] - k
 
             sum_i += sum_j
@@ -68,21 +102,27 @@ if __name__ == "__main__":
     
     n = 5000
     X, P = np.random.rand(n, 100), np.random.rand(n, 2)
-
-    t0 = perf_counter()
-    tw_chunks_jobs = Trustworthiness().compute(X, P, k=20, chunk_size=10, chunk_search=False, n_jobs=-1)
-    print(f"tw_chunks_jobs = {tw_chunks_jobs}, time = {perf_counter() - t0}")
-
-    t0 = perf_counter()
-    tw_chunks = Trustworthiness().compute(X, P, k=20)
-    print(f"tw_chunks = {tw_chunks}, time = {perf_counter() - t0}")
-
-    print("Same?", "Yes" if np.isclose(tw_chunks_jobs, tw_chunks) else "No")
+    k = int(np.sqrt(n))
     
-    t0 = perf_counter()    
+    t0 = perf_counter()
     D_h, D_l = squareform(pdist(X)), squareform(pdist(P))
-    tw_full = trustworthiness(D_h, D_l, k=20)
+    tw_full = trustworthiness(D_h, D_l, k=k)
     print(f"tw_full = {tw_full}, time = {perf_counter() - t0}")
 
+    # Compile
+    # numba_tw(D_h, D_l, k=k)
+    # t0 = perf_counter()
+    # tw_numba = numba_tw(D_h, D_l, k=k)
+    # print(f"tw_numba = {tw_full}, time = {perf_counter() - t0}")
+    # print("Same?", "Yes" if np.isclose(tw_full, tw_numba) else "No")
+
+    t0 = perf_counter()
+    tw_chunks = Trustworthiness().compute(X, P, k=k)
+    print(f"tw_chunks = {tw_chunks}, time = {perf_counter() - t0}")
     # Equal up to 8 decimals
     print("Same?", "Yes" if np.isclose(tw_chunks, tw_full) else "No")
+
+    t0 = perf_counter()
+    tw_chunks_jobs = Trustworthiness().compute(X, P, k=k, n_jobs=-1)
+    print(f"tw_chunks_jobs = {tw_chunks_jobs}, time = {perf_counter() - t0}")
+    print("Same?", "Yes" if np.isclose(tw_chunks_jobs, tw_chunks) else "No")
